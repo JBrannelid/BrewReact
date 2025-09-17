da3f70698e351b9dcddb3433ef62452417c58a7e
Johannes Brannelid <83164833+JBrannelid@users.noreply.github.com>
2025-09-17 22:45:55 +0200

    refactor: remove unused getAll and getById methods from customerService

--- src/api/services/customerService.js
+++ src/api/services/customerService.js
@@ -1,9 +1,5 @@
 import axiosInstance from "../config/axiosConfig";
 
 export const customerService = {
-  getAll: () => axiosInstance.get("Customers"),
-
-  getById: (id) => axiosInstance.get(`Customers/${id}`),
-
   create: (data) => axiosInstance.post("Customers", data),
 };

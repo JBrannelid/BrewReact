c8c4409e6ba5ba96bf1b2aee279341bdc315b88a
Johannes Brannelid <83164833+JBrannelid@users.noreply.github.com>
2025-09-17 22:45:56 +0200

    feat: add Redux state management and error handling to useCreateCustomer hook

--- src/hooks/customers/useCreateCustomer.js
+++ src/hooks/customers/useCreateCustomer.js
@@ -1,9 +1,28 @@
+import { useDispatch } from "react-redux";
+import {
+  setError,
+  clearError,
+  setLoading,
+} from "../../redux/reducers/bookingSlice";
 import { customerService } from "../../api/services/customerService";
 
 export default function useCreateCustomer() {
+  const dispatch = useDispatch();
+
   const createCustomer = async (data) => {
-    const response = await customerService.create(data);
-    return response;
+    dispatch(setLoading(true));
+    dispatch(clearError());
+
+    try {
+      const response = await customerService.create(data);
+      return response;
+    } catch (error) {
+      const errorMessage = error.userMessage || "Failed to create customer";
+      dispatch(setError(errorMessage));
+      return null;
+    } finally {
+      dispatch(setLoading(false));
+    }
   };
 
   return { createCustomer };
